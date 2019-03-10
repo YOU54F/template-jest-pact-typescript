fe917e1e5ceef355f891133f69382c3bb073f057
Yousaf Nabi <yousaf.nabi@infinityworks.com>
2019-03-10 21:01:24 +0000

    skip cert validation for pact upload

--- src/pact/tasks/delete-pacts-broker.sh
+++ src/pact/tasks/delete-pacts-broker.sh
@@ -2,7 +2,7 @@
 # This script will delete a pacticipant - use with care
 for f in src/pact/pacts/*.json; do
   pacticipant=$(jq '.consumer.name' $f | sed s'/"//g')
-  curl -X DELETE \
+  curl -k -X DELETE \
     -u $PACT_BROKER_BASIC_AUTH_USERNAME:$PACT_BROKER_BASIC_AUTH_PASSWORD \
     $PACT_BROKER_URL/pacticipants/$pacticipant
 done
--- src/pact/tasks/tag-pacts-consumer.sh
+++ src/pact/tasks/tag-pacts-consumer.sh
@@ -8,7 +8,7 @@ for f in src/pact/pacts/*.json; do
   git_branch=$(git rev-parse --abbrev-ref HEAD | sed s'/[/]/%2f/g' )
   consumer_version=${CIRCLE_BUILD_NUM}-$git_commit_hash
   
-  curl -X PUT \-H "Content-Type: application/json" \
+  curl -k -X PUT \-H "Content-Type: application/json" \
     -d @$f \
     -u $PACT_BROKER_BASIC_AUTH_USERNAME:$PACT_BROKER_BASIC_AUTH_PASSWORD \
     $PACT_BROKER_URL/pacticipants/$consumer/versions/$consumer_version/tags/$git_branch
