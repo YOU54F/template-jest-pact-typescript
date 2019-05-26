0654856844a75729606a4319e60c82f12f9f6443
YOU54F <yousafn@gmail.com>
2019-05-26 21:45:25 +0100

    install awscli with pip

--- src/pact/verifier/verify.sh
+++ src/pact/verifier/verify.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -o pipefail
 
-
-  if [ ! -f ~/.aws/credentials ]; then
-    echo "AWS credentials not configured!"
-    return 1
-  fi  
-
   AWS_TEMP_CREDS=`aws sts assume-role --role-arn $ARN_ROLE --role-session-name api-gateway-access| jq -c '.Credentials'`
   export AWS_ACCESS_KEY_ID=`echo $AWS_TEMP_CREDS | jq -r '.AccessKeyId'`
   export AWS_SECRET_ACCESS_KEY=`echo $AWS_TEMP_CREDS | jq -r '.SecretAccessKey'`
