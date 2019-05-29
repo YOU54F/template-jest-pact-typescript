8782404a8886a46faad5d68376c898d7bae8e888
YOU54F <yousafn@gmail.com>
2019-05-29 22:44:38 +0100

    reinstate repo and merge changes

---
+++ README.md
@@ -0,0 +1,10 @@
+<<<<<<< Updated upstream
+=======
+
+
+## Credits
+
+- [Pact Foundation](https://github.com/pact-foundation)
+- [Pact JS](https://github.com/pact-foundation/pact-js)
+- [Initial Proposal for Jest-Pact](https://github.com/pact-foundation/pact-js/issues/215#issuecomment-437237669) by [TimothyJones](https://github.com/TimothyJones)
+>>>>>>> Stashed changes
--- src/pact/verifier/verify.sh
+++ src/pact/verifier/verify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o pipefail
 
-  AWS_TEMP_CREDS=`aws sts assume-role --role-arn $ARN_ROLE --role-session-name api-gateway-access| jq -c '.Credentials'`
+  AWS_TEMP_CREDS=`aws sts assume-role --role-arn $ARN_ROLE --role-session-name api-gateway-access | jq -c '.Credentials'`
   export AWS_ACCESS_KEY_ID=`echo $AWS_TEMP_CREDS | jq -r '.AccessKeyId'`
   export AWS_SECRET_ACCESS_KEY=`echo $AWS_TEMP_CREDS | jq -r '.SecretAccessKey'`
   export AWS_SESSION_TOKEN=`echo $AWS_TEMP_CREDS | jq -r '.SessionToken'`
