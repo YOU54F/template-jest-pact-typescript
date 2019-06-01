43e7b9568c0fe91b621f72a8e31c2e00ecea2224
YOU54F <yousafn@gmail.com>
2019-06-01 01:42:22 +0100

    test: query param example

--- src/pact/client/params.pacttest.ts
+++ src/pact/client/params.pacttest.ts
@@ -1,5 +1,5 @@
 import { InteractionObject } from "@pact-foundation/pact";
-import { somethingLike } from "@pact-foundation/pact/dsl/matchers";
+import { regex, term } from "@pact-foundation/pact/dsl/matchers";
 import * as jestpact from "jest-pact";
 import * as supertest from "supertest";
 const port = 9999;
@@ -22,7 +22,10 @@ jestpact.pactWith(
             method: "GET",
             path: "/notifications",
             query: {
-              signingId: somethingLike("9eb2484d-405e-4ff7-bc3a-b0181e4efb30")
+              signingId: regex({
+                generate: "9eb2484d-405e-4ff7-bc3a-b0181e4efb30",
+                matcher: "\\d+"
+              })
             }
           },
           willRespondWith: {
@@ -37,7 +40,42 @@ jestpact.pactWith(
         await provider.addInteraction(interaction);
 
         await getClient()
-          .get("/notifications?signingId=9eb2484d-405e-4ff7-bc3a-b0181e4efb30")
+          .get("/notifications?signingId=123")
+          .expect(200);
+      });
+    });
+
+    describe("path param matcher test", () => {
+      it("should respond 200 when a notification is send", async () => {
+        // const signingId = regex({generate: '9eb2484d-405e-4ff7-bc3a-b0181e4efb30', matcher: "\\d+"})
+
+        const interaction: InteractionObject = {
+          state: "Service is up and healthy",
+          uponReceiving: "a notification",
+          withRequest: {
+            method: "GET",
+            path: term({
+              generate: "/notifications?signingId=123",
+              matcher: "/notifications\\?signingId\\=\\d+"
+            })
+            // query: {
+            //   signingId:
+            // }
+          },
+          willRespondWith: {
+            status: 200,
+            body: "",
+            headers: {
+              "Content-Type": "application/json"
+            }
+          }
+        };
+
+        await provider.addInteraction(interaction);
+
+        await getClient()
+          .get("/notifications")
+          .query("signingId=123")
           .expect(200);
       });
     });
